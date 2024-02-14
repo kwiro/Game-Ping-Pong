6f9722c52d776a84cbfce494c6dfefa217a0122e
kwiro <159569222+kwiro@users.noreply.github.com>
2024-02-14 20:45:18 +0300

    v2
    add class Player and add 2 rocket

--- ping_pong.py
+++ ping_pong.py
@@ -7,9 +7,26 @@ class Game_sprite(sprite.Sprite):
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
-        self.tect.y = player_y
+        self.rect.y = player_y
     def rezet(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Player(Game_sprite):
+    def update_r(self):
+        case = key.get_pressed()
+        if case[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if case[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed 
+    def update_l(self):
+        case = key.get_pressed()
+        if case[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if case[K_s] and self.rect.y < 420:
+            self.rect.y += self.speed 
+
+roket1 = Player('left.png',30, 200, 4, 50, 100)
+roket2 = Player('Right.png', 520, 200, 4, 50, 100)
 
 back = (51, 148, 204)
 window = display.set_mode((600, 500))
@@ -29,5 +46,9 @@ while game:
             game = False
     if finish != True:
         window.fill(back)
+        roket1.update_l()
+        roket2.update_r()
+        roket1.rezet()
+        roket2.rezet()
     display.update()
     clock.tick(fps)
